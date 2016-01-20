5b2284cd4004a7ea58f831b0db9df7caf47995a7
Mateus Pires <mateusplpl@gmail.com>
2016-01-20 00:06:44 -0200

    Usaco: test e ride

---
+++ main.cpp
@@ -0,0 +1,32 @@
+/*
+ID: mateusp1
+PROG: ride
+LANG: C++
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ofstream fout ("ride.out");
+	ifstream fin ("main.in");
+
+	char comet[8], group[8];
+	fin >> comet >> group;
+
+	int cometCode = 1;
+	for (int i = 0; i < 6 && comet[i] != '\n' && comet[i] != '\0'; i++)
+		cometCode *= comet[i] - 'A' + 1;
+
+	int groupCode = 1;
+	for (int i = 0; i < 6 && group[i] != '\n' && group[i] != '\0'; i++)
+		groupCode *= group[i] - 'A' + 1;
+
+	fout << (cometCode % 47 == groupCode % 47 ? "GO" : "STAY") << "\n";
+
+	return 0;
+}
