9c7eae429ddb0eb3503d8b1623256123075fae3b
Mateus Pires <mateusplpl@gmail.com>
2016-01-23 17:55:53 -0200

    Section 1.1: friday done, beads done

--- Usaco/Section 1.1/friday/friday.cpp
+++ Usaco/Section 1.1/friday/friday.cpp
@@ -16,6 +16,10 @@ const int YEAR_START = 1900;
 const int YEAR = 12;
 const int MONTHS[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+int update(int week[], int day) {
+	week[(WEEK_START + day + 11) % WEEK]++;
+}
+
 int main()
 {
 	ofstream fout ("friday.out");
@@ -24,61 +28,27 @@ int main()
 	int n;
 	fin >> n;
 
-	int week[7];
+	int week[WEEK] = {0};
 	int end = YEAR_START + n - 1;
 	int day = 1;
-	int weekDay = WEEK_START;
 
 	for (int y = YEAR_START; y <= end; y++)
 	{
-		for (int)
+		update(week, day);
+		day += MONTHS[0];
 
-		day = 13;
-		week[day / WEEK + day % WEEK]++;
+		update(week, day);
+		day += MONTHS[1];
 
-		day = 31 + 13;
-		week[day / WEEK + day % WEEK]++;
+		if (y % 4 == 0 && (y % 100 != 0 || y % 400 == 0))
+			day++;
 
-		day = 31;
-
-		if (y % 100 == 0 && y % 400 == 0)
-			day += 29;
-
-		else if (y % 100 > 0 && y % 4 == 0)
-			day += 29;
-		
-		else
-			day += 28;
-
-		
-
+		for (int m = 2; m < YEAR; m++)
+		{
+			update(week, day);
+			day += MONTHS[m];
+		}
 	}
-
-
-
-
-	int days = 0;
-
-	for (int y = YEAR_START; y <= end; y++)
-	{
-		if (y % 100 == 0 && y % 400 == 0)
-			days += 366;
-
-		else if (y % 100 > 0 && y % 4 == 0)
-			days += 366;
-		
-		else
-			days += 365;
-	}
-
-	int all = days / 7;
-	int mod = days % 7;
-
-	for (int d = WEEK_START; d < WEEK; d++)
-		week[d] = all + (mod-- > 0 ? 1 : 0);
-
-	for (int d = 0; d < WEEK_START; d++)
-		week[d] = all + (mod-- > 0 ? 1 : 0);
 
 	for (int d = 0; d < WEEK - 1; d++)
 		fout << week[d] << " ";
