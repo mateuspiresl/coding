737a6c790de4f6e635ad3ef51f36f40fa41a8ca6
Mateus Pires <mateusplpl@gmail.com>
2016-01-26 16:42:46 -0200

    URI: 1033, 1034 done

--- URI/Paradigmas/1033.cpp
+++ URI/Paradigmas/1033.cpp
@@ -4,16 +4,17 @@ using namespace std;
 
 typedef unsigned long long ull;
 
-ull countCalls(ull, ull);
+int countCalls(ull, int);
+int** matrixProduct(int**, int**, int);
+int callsFor(int**, int);
+
+int MATRIX[3][3] = { { 1, 1, 1 }, { 1, 0, 0 }, { 0, 0, 1 } };
 
 int main()
 {
 	int tc = 0;
 
-	for (int i = 0; i < 100; i++)
-		cout << countCalls(i, 9999999) << endl;
-
-	while (false)//(++tc)
+	while (++tc)
 	{
 		ull num;
 		int base;
@@ -30,17 +31,72 @@ int main()
 	return 0;
 }
 
-ull countCalls(ull number, ull base)
+int countCalls(ull number, int base)
 {
-	ull a = 1ull,
-		b = 1ull;
+	if (number == 0ull || number == 1ull)
+		return 1;
 
-	for (ull i = 2ull; i <= number; i++)
+	int** cache[64] = {NULL};
+	int calculated = 0;
+
+	cache[0] = new int*[3];
+	cache[0][0] = MATRIX[0];
+	cache[0][1] = MATRIX[1];
+	cache[0][2] = MATRIX[2];
+
+	number--;
+
+	int bit = 63;
+
+	while (number >> bit == 0x0) bit--;
+
+	if (bit > calculated)
 	{
-		ull r = (a + b + 1ull) % base;
-		a = b;
-		b = r;
+		int b = calculated + 1; 
+
+		for (; b <= bit; b++)
+			cache[b] = matrixProduct(cache[b - 1], cache[b - 1], base);
+
+		calculated = b - 1;
 	}
 
-	return b;
+	int** result = cache[bit];
+
+	while (--bit >= 0)
+		if (number & (0x1 << bit))
+			result = matrixProduct(result, cache[bit], base);
+
+	return callsFor(result, base);
+}
+
+int** matrixProduct(int** a, int** b, int base)
+{
+	int** result = new int*[3];
+
+	for (int l = 0; l < 3; l++)
+	{
+		result[l] = new int[3];
+
+		for (int c = 0; c < 3; c++)
+		{
+			result[l][c] = (
+				a[l][0] * b[0][c] +
+				a[l][1] * b[1][c] +
+				a[l][2] * b[2][c]
+			) % base;
+		}
+	}
+
+	return result;
+}
+
+int callsFor(int** matrix, int base)
+{
+	int result[3] = {0};
+
+	for (int l = 0; l < 3; l++)
+		for (int c = 0; c < 3; c++)
+			result[l] += matrix[l][c];
+	
+	return result[0] % base;
 }
---
+++ URI/Paradigmas/1034.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+	int numDays;
+
+	while (cin >> numDays)
+	{
+		int cost;
+		cin >> cost;
+
+		int days[numDays];
+
+		for (int i = 0; i < numDays; i++)
+			cin >> days[i];
+		
+		
+	}
+
+	return 0;
+}
