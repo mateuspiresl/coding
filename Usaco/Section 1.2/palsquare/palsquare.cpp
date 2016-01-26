ec16daf5f46bb6870dab411f3ea5b0a68d39a5d1
Mateus Pires <mateusplpl@gmail.com>
2016-01-25 22:24:07 -0200

    Section 1.2: palsquare done

--- Usaco/Section 1.2/palsquare/palsquare.cpp
+++ Usaco/Section 1.2/palsquare/palsquare.cpp
@@ -7,19 +7,15 @@ LANG: C++
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <map>
-#include <vector>
-#include <algorithm>
-#include <math.h>
 
 using namespace std;
 
-const int REPRESENTATIONS[] = {
-	"1", "2", "3", "4", "5", "6", "7", "8", "9",
-	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"
+const char REPRESENTATIONS[] = {
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'
 };
 
-string convert(int base, string value);
+string convert(int base, int value);
 bool palindrome(string value);
 
 int main()
@@ -32,18 +28,36 @@ int main()
 
 	for (int i = 1; i <= 300; i++)
 	{
-		
+		string number = convert(base, i * i);
+
+		if (palindrome(number))
+			fout << convert(base, i) << " " << number << "\n";
 	}
 
 	return 0;
 }
 
-string convert(int base, string value)
+string convert(int base, int value)
 {
-	
+	string result = "";
+
+	while (value > 0)
+	{
+		result = REPRESENTATIONS[value % base] + result;
+		value /= base;;
+	}
+
+	return result;
 }
 
 bool palindrome(string value)
 {
-	
+	string::iterator begin = value.begin();
+	string::iterator end = value.end() - 1;
+
+	while (begin < end)
+		if (*begin++ != *end--)
+			return false;
+
+	return true;
 }
