299b8c0873081a22457daa1668b3f27087e6fae3
Mateus Pires <mateusplpl@gmail.com>
2016-05-27 01:36:08 -0300

    UVA: 11475

--- Judges/UVA/11475.cpp
+++ Judges/UVA/11475.cpp
@@ -8,27 +8,60 @@ int main()
 
 	while (cin >> s)
 	{
-		bool odd = s.length() % 2 != 0;
-		int half = s.length() >> 1; 
-		int len = len;
+		bool even = s.length() % 2 == 0;
+		int left = (s.length() >> 1) - 1;
+		int right = left + (even ? 1 : 2);
 
-		for (; len <= max; len++)
+		while (right < s.length())
 		{
-			int left = len - 1;
-			int right = odd ? len + 1 : len;
+			/*string s2 = s;
+			s2[left] += 'A' - 'a';
+			s2[right] += 'A' - 'a';
+			cout << "~ " << s2 << endl;*/
 
-			for (; right < s.length(); left--, right++)
-				if (s[left] != s[right])
+			if (s[left] == s[right])
+			{
+				int l = left - 1;
+				int r = right + 1;
+
+				while (r < s.length())
+				{
+					if (s[l] != s[r])
+						break;
+					else
+					{
+						l--;
+						r++;
+					}
+				}
+
+				if (r == s.length())
+				{
+					left -= s.length() - right;
 					break;
-
-			if (right == s.length())
-				break;
-		}
-
-		if (len != half)
-		{
+				}
+			}
 			
+			if (even)
+				right++;
+			else
+				left++;
+
+			even = ! even;
 		}
+
+		//cout << "~ " << left << endl;
+
+		cout << s;
+
+		if (left >= 0)
+		{
+			string concat = s.substr(0, left + 1);
+			reverse(concat.begin(), concat.end());
+			cout << concat;
+		}
+		
+		cout << endl;
 	}
 
 	return 0;
